3bfb0c337f92d4ec89babb12398fc7e87fb34da9
Timofii0507 <timofiiyurkov@gamil.com>
2023-12-24 19:47:39 +0200

    Четверте  завдання  з практичної роботи

---
+++ lab.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int num1, num2;
+    cout << "Введіть перше число: ";
+    cin >> num1;
+    cout << "Введіть друге число: ";
+    cin >> num2;
+    int* ptr1 = &num1;
+    int* ptr2 = &num2;
+    if (*ptr1 > *ptr2) 
+    {
+        cout << "Найбільше число: " << *ptr1 << endl;
+    }
+    else if (*ptr2 > *ptr1) 
+    {
+        cout << "Найбільше число: " << *ptr2 << endl;
+    }
+    else 
+    {
+        cout << "Числа рівні" << endl;
+    }
+    return 0;
+}
